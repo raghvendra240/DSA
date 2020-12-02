92a6bc74aa9bf42cbd9d46fae272eadd9629f8e0
raghvendra240 <55599280+raghvendra240@users.noreply.github.com>
2020-12-02 16:08:46 +0530

    Create Kadane's Algorithm .cpp

---
+++ Dynamic programming/Kadane's Algorithm .cpp
@@ -0,0 +1,15 @@
+int maxSubarraySum(int arr[], int n){
+    
+    int res=arr[0];
+    int maxending=arr[0];
+    
+    
+    for(int i=1;i<n;i++)
+    {
+        maxending=max(maxending+arr[i],arr[i]);
+        res=max(res,maxending);
+    }
+    
+    
+    return res;
+}
