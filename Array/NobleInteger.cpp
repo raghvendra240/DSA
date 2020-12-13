413beacb7824558002eaf47a50104912a10b8e8d
raghvendra240 <55599280+raghvendra240@users.noreply.github.com>
2020-12-13 21:25:17 +0530

    Create NobleInteger.cpp

---
+++ Array/NobleInteger.cpp
@@ -0,0 +1,22 @@
+
+link->https://www.interviewbit.com/problems/noble-integer/
+
+int Solution::solve(vector<int> &A) {
+    
+    int n=A.size();
+    sort(A.begin(),A.end());
+   
+  for(int i=0;i<n;i++)
+  {
+      if(A[i]<0)
+        continue;
+       
+       if(i<n-1 && A[i]==A[i+1]) 
+         continue;
+       
+      if(A[i]==(n-i-1))
+         return 1;
+  }
+  
+  return -1;
+}
