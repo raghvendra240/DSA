5468e103685955122ec5a34920ead21a21302a49
raghvendra240 <55599280+raghvendra240@users.noreply.github.com>
2022-01-14 21:13:35 +0530

    Time: 36 ms (23.69%), Space: 15.5 MB (91.20%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -10,28 +10,20 @@
  * };
  */
 class Solution {
-    vector<int>dp;
 public:
-    int pathSum(TreeNode* root, int targetSum) {
-        
-        if(root==NULL)
-            return 0;
-        
-        int index=dp.size()-1;
-        int sum=root->val;
-        int res=0;
-        while(index>=0){
-            if(sum==targetSum)
-                res++;
-            sum+=dp[index];
-            index--;
+     int ans=0;
+    int pathSum(TreeNode* root, int sum) {
+        if(root){
+            dfs(root,sum);
+            pathSum(root->left,sum);
+            pathSum(root->right,sum);
         }
-        if(sum==targetSum)
-            res++;
-        dp.push_back(root->val);
-        int left=pathSum(root->left,targetSum);
-        int right=pathSum(root->right,targetSum);
-        dp.pop_back();
-        return left+right+res;
+        return ans;
+    }
+    void dfs(TreeNode* root, int sum){
+        if(!root)return;
+        if(root->val==sum)ans++;
+        dfs(root->left,sum-root->val);
+        dfs(root->right,sum-root->val);
     }
 };
