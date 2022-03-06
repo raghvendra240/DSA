91fa7ac8a432b7e02cd387f4cc8527c170d0a166
raghvendra240 <55599280+raghvendra240@users.noreply.github.com>
2022-03-06 22:51:58 +0530

    Time: 95 ms (73.09%), Space: 61.2 MB (70.86%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.cpp
@@ -11,6 +11,18 @@ int numSubarrayProductLessThanK(vector<int>& nums, int k) {
             p/=nums[start++];
         }
         
+        /*
+        Let's say the previous values are [x,y] and we now include current value i.e, z to it   
+        as already covered subarrays till y in our ans, so only the new ones
+        
+        x,y,z
+        y,z
+        z,
+        *****we can not add x,z [not subarray]
+
+          so res= end-start+1
+        */
+        
         res+=(end-start+1);
     }
     return res;
