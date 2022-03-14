b694186db4f8ae14197482551fbf1a94a94e7158
raghvendra240 <55599280+raghvendra240@users.noreply.github.com>
2022-03-14 23:53:19 +0530

    Time: 485 ms (10.44%), Space: 69 MB (12.48%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+     unordered_map<int,vector<int>>map;
+ vector<int>outDeg;
+ vector<bool>tempRes;
+ vector<bool>vis;
+ bool help(int u){
+   vis[u]=true;
+   if(outDeg[u]==0)
+   {
+       tempRes[u]=true;
+       return true;
+   }
+   bool res=true;
+   for(auto v : map[u]){
+      if(vis[v]==false){
+         bool temp = help(v);
+         if(temp==false)
+             res=false;
+            
+      }else if(tempRes[v]==false){
+          res=false;
+      }
+   }
+   tempRes[u]=res;
+   return res;
+ }
+ vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+ int n=graph.size();
+ outDeg.resize(n,0);
+ vis.resize(n,false);
+ tempRes.resize(n,false);
+for(int i=0;i<n;i++){
+   for(int j=0;j<graph[i].size();j++){
+      map[i].push_back(graph[i][j]);
+      outDeg[i]++;
+   }
+} 
+vector<int>res;
+for(int i=0;i<n;i++){
+   if(vis[i]==false){
+       // cout<<i<<": "<<outDeg[i]<<endl;
+      help(i);
+      // if(temp)
+   }
+}
+for(int i=0;i<tempRes.size();i++){
+  if(tempRes[i])
+     res.push_back(i);
+}
+return res;
+}
+};
