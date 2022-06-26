301fac8d6970e95dc2e432a5124a85dd6fcc3c87
raghvendra240 <55599280+raghvendra240@users.noreply.github.com>
2022-06-26 16:25:55 +0530

    Time: 590 ms (5.06%), Space: 18.5 MB (83.54%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
@@ -6,9 +6,11 @@ public:
         for(int i = 0 ;i < n ; i++){
             int j = i -1 ;
             while(j>=0 && arr[j] < arr[j+1]) --j;
+            if(abs(i-j) < 2) continue;
             int k = i+1;
             while(k<n && arr[k] < arr[k-1]) k++;
-            if(abs(i-j)>1 && abs(i-k)>1) res = max(res, k-j-1);
+           if(abs(i-k) < 2 ) continue;
+            res = max(res, k-j-1);
         }
         return res<3 ? 0 : res;
         
