c692c2526f471c6e97ae61a137d56cbd290b4bc4
raghvendra240 <55599280+raghvendra240@users.noreply.github.com>
2022-10-23 18:26:04 +0530

    Time: 25 ms (94.90%), Space: 11 MB (82.33%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -2,40 +2,18 @@ class Solution {
 public:
     int minFlipsMonoIncr(string s) {
         
-      int n  = s.size();
-      int zerosTillNow[n];
-      int onesTillNow[n];
-      int totalZeros = 0;
-      int totalOnes = 0;
-      zerosTillNow[0] = 0;
-      onesTillNow[0] = 0;
-      for (int i = 0; i < n; i++) {
-          if (i > 0) {
-             onesTillNow[i] = onesTillNow[i-1];
-             zerosTillNow[i] = zerosTillNow[i-1];  
-          }
-        if (s[i] == '0') {
-            zerosTillNow[i] += 1 ;
-            totalZeros++;
-        } else {
-            onesTillNow[i] += 1;
-            totalOnes++;
+        int ones = 0;
+        int flips = 0;
+        for (auto ch : s){
+            
+            if (ch == '1') {
+                ++ones;
+            } else {
+                flips++;
+                flips  = min (flips,ones);
+            }
         }
-          // cout<<zerosTillNow[i]<<":"<<onesTillNow[i]<<endl;
-      }
-
-      int res = min(totalOnes, totalZeros);
-      if (totalOnes == 0 || totalZeros == 0) return 0;
-
-      for (int i = 0; i <n;i++){
-            int onesOnLeft  = onesTillNow[i];
-            int zerosOnRight = totalZeros - zerosTillNow[i];
-          // cout<<totalZeros<<"-"<<zerosTillNow[i]<<endl;
-          // cout<<onesOnLeft<<"--"<<zerosOnRight<<endl;
-            res = min(res,onesOnLeft+zerosOnRight);
-      }
-
-      return res;
+        return flips;
         
     }
 };
